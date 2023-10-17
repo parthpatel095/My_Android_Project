971c759991b9711391ddeffbbf84cb17d822d778
HP <parthpatel18122@gmail.com>
2023-10-17 10:13:57 +0530

    project 1

---
+++ app/src/main/java/com/example/myandroidproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myandroidproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,HomeActivity::class.java))
+            finish()
+        } ,3000)
+    }
+
+}
---
+++ app/src/main/java/com/example/myandroidproject/Music1.kt
@@ -0,0 +1,11 @@
+package com.example.myandroidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Music1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music1)
+    }
+}
