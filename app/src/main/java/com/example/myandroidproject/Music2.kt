2f068abca50c83d4b869c12953a7bfb269109bfa
HP <parthpatel18122@gmail.com>
2023-10-19 21:57:34 +0530

    project

---
+++ app/src/main/java/com/example/myandroidproject/Music2.kt
@@ -0,0 +1,11 @@
+package com.example.myandroidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Music2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music2)
+    }
+}
