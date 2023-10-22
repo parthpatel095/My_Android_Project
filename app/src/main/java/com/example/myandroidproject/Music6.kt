c426df75f97ebc762c3820eea69bbcd9b0f027ba
HP <parthpatel18122@gmail.com>
2023-10-22 09:25:56 +0530

    project

---
+++ app/src/main/java/com/example/myandroidproject/Music6.kt
@@ -0,0 +1,11 @@
+package com.example.myandroidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Music6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music6)
+    }
+}
--- app/src/main/java/com/example/myandroidproject/Music_mainActivity.kt
+++ app/src/main/java/com/example/myandroidproject/Music_mainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.myandroidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Music_mainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_main)
+
+        val Play1 : Button =findViewById(R.id.Button1)
+        Play1.setOnClickListener {
+            Intent(this,Music1::class.java).also { startActivity(it) }
+        }
+
+        val Play2 : Button =findViewById(R.id.Button2)
+        Play2.setOnClickListener {
+            Intent(this,Music2::class.java).also { startActivity(it) }
+        }
+
+        val Play3 : Button =findViewById(R.id.Button3)
+        Play3.setOnClickListener {
+            Intent(this,Music3::class.java).also { startActivity(it) }
+        }
+
+        val Play4 : Button =findViewById(R.id.Button4)
+        Play4.setOnClickListener {
+            Intent(this,Music4::class.java).also { startActivity(it) }
+        }
+
+        val Play5 : Button =findViewById(R.id.Button5)
+        Play5.setOnClickListener {
+            Intent(this,Music5::class.java).also { startActivity(it) }
+        }
+
+        val Play6 : Button =findViewById(R.id.Button6)
+        Play6.setOnClickListener {
+            Intent(this,Music6::class.java).also { startActivity(it) }
+        }
     }
 }
